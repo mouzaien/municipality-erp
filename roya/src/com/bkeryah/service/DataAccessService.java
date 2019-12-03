500f1c2ae166c4d2d856f8dd2df9fe79c4e142f7
haithamsaid <57109351+haithamsaid@users.noreply.github.com>
2019-12-03 09:56:42 +0300

    bugs merge

--- roya/src/com/bkeryah/service/DataAccessService.java
+++ roya/src/com/bkeryah/service/DataAccessService.java
@@ -3829,6 +3829,7 @@ public class DataAccessService implements IDataAccessService {
 			try {
 				return commonDao.saveObject(myObject);
 			} catch (Exception e) {
+				e.printStackTrace();
 				logger.error(
 						"save action primary key  :" + myObject.getClass().getSimpleName() + "  " + e.getMessage());
 				failCount++;
@@ -4324,7 +4325,7 @@ public class DataAccessService implements IDataAccessService {
 
 		List<Integer> attachmentIds = addAttachments(attachs);
 
-		if (!isForSave) {
+//		if (!isForSave) {
 			if (wrkApplication.getId() != null) {
 
 				List<WrkApplication> allRecordWrkApplications = commonDao
@@ -4339,7 +4340,7 @@ public class DataAccessService implements IDataAccessService {
 				wrkApplication.getId().setStepId(wrkApplication.getId().getStepId() + 1);
 			}
 
-		}
+//		}
 		AddNewInternalMemo(wrkApplication, arcRecord, attachmentIds);
 
 		for (String id : toCopyList) {
